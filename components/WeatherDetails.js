47a5ffe04b79e81e9fc02745d0ab3a2fc2fdc68a
Prathamesh Sai <prathameshsaisankar@Gmail.com>
2021-05-13 22:43:58 +0100

    Update WeatherDetails.js

--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -1,61 +1,56 @@
 import React from 'react'
-import { View, Text, StyleSheet} from 'react-native'
-import {colors} from '../utils/index'
-import {FontAwesome5, MaterialCommunityIcons} from '@expo/vector-icons'
+import { View, Text, StyleSheet } from 'react-native'
+import { colors } from '../utils/index'
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons'
 
-//get the colors from index.js
 const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors
 
-export default function WeatherDetails({currentWeather, unitsSystem}) {
-    //split our data to get specific parts of it.
-    const{
-        main: {feels_like, humidity, pressure },
+export default function WeatherDetails({ currentWeather, unitsSystem }) {
+    const {
+        main: { feels_like, humidity, pressure },
         wind: { speed }
     } = currentWeather
-
-    //get the windspeed according to the user's preferences.
     const windSpeed = unitsSystem == 'metric' ? `${Math.round(speed)} metres/s` : `${Math.round(speed)} miles/h`
-
     return (
         <View style={styles.weatherDetails}>
             <View style={styles.weatherDetailsRow}>
-                <View style={{...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR}}>
-                <View style={styles.weatherDetailsRow}>
-                <FontAwesome5 name="temperature-high" size={30} color={PRIMARY_COLOR} />
-                <View style={styles.weatherDetailsItems}>
-                <Text>Feels like:     </Text>
-                <Text style={styles.textSecondary}>{feels_like}°      </Text>
-                </View>
-                </View>
+                <View style={{ ...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR }}>
+                    <View style={styles.weatherDetailsRow}>
+                        <FontAwesome5 name="temperature-high" size={30} color={PRIMARY_COLOR} />
+                        <View style={styles.weatherDetailsItems}>
+                            <Text>Feels like:     </Text>
+                            <Text style={styles.textSecondary}>{feels_like}°      </Text>
+                        </View>
+                    </View>
                 </View>
                 <View style={styles.weatherDetailsBox}>
-                <View style={styles.weatherDetailsRow}>
-                <FontAwesome5 name="tint" size={30} color={PRIMARY_COLOR} />
-                <View style={styles.weatherDetailsItems}>
-                <Text>Humidity:     </Text>
-                <Text style={styles.textSecondary}>{humidity}%      </Text>
-                </View>
-                </View>
+                    <View style={styles.weatherDetailsRow}>
+                        <FontAwesome5 name="tint" size={30} color={PRIMARY_COLOR} />
+                        <View style={styles.weatherDetailsItems}>
+                            <Text>Humidity:     </Text>
+                            <Text style={styles.textSecondary}>{humidity}%      </Text>
+                        </View>
+                    </View>
                 </View>
             </View>
-            <View style={{...styles.weatherDetailsRow, borderTopWidth: 1, borderTopColor: BORDER_COLOR}}>
-                <View style={{...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR}}>
-                <View style={styles.weatherDetailsRow}>
-                <FontAwesome5 name="wind" size={30} color={PRIMARY_COLOR} />
-                <View style={styles.weatherDetailsItems}>
-                <Text>Wind Speed:   </Text>
-                <Text style={styles.textSecondary}>{windSpeed}   </Text>
-                </View>
-                </View>
+            <View style={{ ...styles.weatherDetailsRow, borderTopWidth: 1, borderTopColor: BORDER_COLOR }}>
+                <View style={{ ...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR }}>
+                    <View style={styles.weatherDetailsRow}>
+                        <FontAwesome5 name="wind" size={30} color={PRIMARY_COLOR} />
+                        <View style={styles.weatherDetailsItems}>
+                            <Text>Wind Speed:   </Text>
+                            <Text style={styles.textSecondary}>{windSpeed}   </Text>
+                        </View>
+                    </View>
                 </View>
                 <View style={styles.weatherDetailsBox}>
-                <View style={styles.weatherDetailsRow}>
-                <MaterialCommunityIcons style={{left: -7}} name="speedometer" size={35} color={PRIMARY_COLOR} />
-                <View style={styles.weatherDetailsItems}>
-                <Text>Pressure:      </Text>
-                <Text style={styles.textSecondary}>{pressure} hPa    </Text>
-                </View>
-                </View>
+                    <View style={styles.weatherDetailsRow}>
+                        <MaterialCommunityIcons style={{ left: -7 }} name="speedometer" size={35} color={PRIMARY_COLOR} />
+                        <View style={styles.weatherDetailsItems}>
+                            <Text>Pressure:      </Text>
+                            <Text style={styles.textSecondary}>{pressure} hPa    </Text>
+                        </View>
+                    </View>
                 </View>
             </View>
         </View>
@@ -72,7 +67,7 @@ const styles = StyleSheet.create({
         marginTop: -10,
         marginBottom: 140,
     },
-    weatherDetailsRow:{
+    weatherDetailsRow: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
